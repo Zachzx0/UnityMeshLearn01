b48507c2088fe7b45490d73b70c41d895b443041
Zachzx0 <zachzx0@gmail.com>
2018-07-15 19:59:14 +0800

    first commit

---
+++ Assets/Scripts/CreateMeshSphereTest.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class CreateMeshSphereTest : MonoBehaviour
+{
+    MeshFilter myMeshFilter;
+    public float radius = 1;
+    void Start()
+    {
+        myMeshFilter = GetComponent<MeshFilter>();
+
+        Mesh ball = new Mesh();
+
+        //顶点数组
+        Vector3[] ballVertices = new Vector3[182];
+        //三角形数组
+        int[] ballTriangles = new int[1080];
+        /*水平每18度、垂直每18度确定一个顶点，
+        顶部和底部各一个顶点，一共是9x20+2=182个顶点。
+        每一环与相邻的下一环为一组，之间画出40个三角形，一共8组。
+        顶部和底部各与相邻环画20个三角形，总三角形数量40x8+20x2=360,
+        三角形索引数量360x3=1080*/
+
+
+        int verticeCount = 0;
+        for (int vD = 18; vD < 180; vD += 18)
+        {
+            float circleHeight =
+            radius * Mathf.Cos(vD * Mathf.Deg2Rad);
+            float circleRadius =
+            radius * Mathf.Sin(vD * Mathf.Deg2Rad);
+            for (int hD = 0; hD < 360; hD += 18)
+            {
+                ballVertices[verticeCount] =
+                new Vector3(
+                circleRadius * Mathf.Cos(hD * Mathf.Deg2Rad),
+                circleHeight,
+                circleRadius * Mathf.Sin(hD * Mathf.Deg2Rad));
+                verticeCount++;
+            }
+        }
+        ballVertices[180] = new Vector3(0, radius, 0);
+        ballVertices[181] = new Vector3(0, -radius, 0);
+        ball.vertices = ballVertices;
+
+
+        int triangleCount = 0;
+        for (int j = 0; j < 8; j++)
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                ballTriangles[triangleCount++] =
+                j * 20 + i;
+                ballTriangles[triangleCount++] =
+                (j + 1) * 20 + (i == 19 ? 0 : i + 1);
+                ballTriangles[triangleCount++] =
+                (j + 1) * 20 + i;
+                ballTriangles[triangleCount++] =
+                j * 20 + i;
+                ballTriangles[triangleCount++] =
+                j * 20 + (i == 19 ? 0 : i + 1);
+                ballTriangles[triangleCount++] =
+                (j + 1) * 20 + (i == 19 ? 0 : i + 1);
+            }
+        }
+        for (int i = 0; i < 20; i++)
+        {
+            ballTriangles[triangleCount++] =
+            180;
+            ballTriangles[triangleCount++] =
+            (i == 19 ? 0 : i + 1);
+            ballTriangles[triangleCount++] =
+            i;
+            ballTriangles[triangleCount++] =
+            181;
+            ballTriangles[triangleCount++] =
+            160 + i;
+            ballTriangles[triangleCount++] =
+            160 + (i == 19 ? 0 : i + 1);
+        }
+        ball.triangles = ballTriangles;
+        ball.RecalculateNormals();
+        myMeshFilter.mesh = ball;
+    }
+}
+
---
+++ Assets/Scripts/CreateSphere.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateSphere : MonoBehaviour {
+
+    //public GameObject go;
+    public int angleTt = 1;
+    public int angleFy = 1;
+    public float r = 10;
+
+    int _verticlesCount;
+    int _triCount;
+
+    int horizontalNum;
+    int verticalNum;
+
+   List<Vector3> _verticlesList;
+    List<int> _trianglesList;
+    List<Vector2> _uvs;
+
+    const string matPath = "Materials/mat_earth";
+    const string texturePath = "Textures/worldmap";
+
+    void Start()
+    {
+        CreateEarth();
+    }
+
+    void CreateEarth()
+    {
+
+        horizontalNum = 360 / angleTt;
+        verticalNum = 180 / angleFy;
+        _verticlesList = new List<Vector3>();
+        _trianglesList = new List<int>();
+        _uvs = new List<Vector2>();
+
+        GameObject newGO = Instantiate(new GameObject("Earth"),Vector3.zero,Quaternion.identity);
+        newGO.AddComponent<MeshFilter>();
+        newGO.AddComponent<MeshRenderer>();
+
+        Material mat = Resources.Load<Material>(matPath);
+        Texture texEarth = Resources.Load<Texture>(texturePath);
+
+        newGO.GetComponent<MeshRenderer>().material = mat;
+
+        Mesh mesh = newGO.GetComponent<MeshFilter>().mesh;
+        mesh.Clear();
+        CreateObj(ref _verticlesList, ref _trianglesList);
+        CreateUv(ref _uvs);
+        Debug.Log("<color=red>vertListCount:" + _verticlesList.Count + "</color>");
+        Debug.Log("<color=red>_trianglesListCount:" + _trianglesList.Count + "</color>");
+        Debug.Log("<color=red>_uvsCount:" + _uvs.Count + "</color>");
+        mesh.vertices = _verticlesList.ToArray();
+        mesh.triangles = _trianglesList.ToArray();
+        mesh.uv = _uvs.ToArray();
+
+
+    }
+
+
+    void CreateObj(ref List<Vector3> vList, ref List<int> tList)
+    {
+        for (int i =0; i <=360; i += angleFy)
+        {
+            for (int j = 0; j <=360; j += angleTt)
+            {
+
+                float x = r * Mathf.Sin(Deg2Rad(i)) * Mathf.Cos(Deg2Rad(j));
+                float y = r * Mathf.Cos(Deg2Rad(i));
+                float z = r * Mathf.Sin(Deg2Rad(i))* Mathf.Sin(Deg2Rad(j));
+                Vector3 pos = new Vector3(x, y, z);
+                vList.Add(pos);
+                //Instantiate(go, pos, Quaternion.identity);
+                //Debug.Log("pos:" + pos.ToString());
+            }
+        }
+
+        for(int i = 0; i < verticalNum+1; i++)
+        {
+            for(int j = 0; j < horizontalNum+1; j++)
+            {
+                tList.Add(i * horizontalNum + j);
+                tList.Add((i + 1) * horizontalNum + (j+1));
+                tList.Add((i+1) * horizontalNum + j);
+                tList.Add((i + 1) * horizontalNum +(j + 1));
+                tList.Add(i * horizontalNum + j);
+                tList.Add(i * horizontalNum + (j + 1));
+            }
+        }
+    }
+
+    void CreateUv(ref List<Vector2> uvs)
+    {
+        float verticalOffset = 1.0f / verticalNum;
+        float horizontalOffset = 1.0f / horizontalNum;
+
+        for(int i = 0; i < verticalNum *2 +1; i++)
+        {
+            for(int j = 0; j < horizontalNum+1; j++)
+            {
+                uvs.Add(new Vector2(j * horizontalOffset, i*verticalOffset));
+            }
+        }
+    }
+
+    float Deg2Rad(float angle)
+    {
+        return angle * Mathf.Deg2Rad;
+    }
+}
